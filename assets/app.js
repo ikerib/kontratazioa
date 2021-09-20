95fa7500c5eca1a42b7ce26fa3d9741f3f56748c
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2021-09-20 12:30:09 +0200

    Fontawesome

--- assets/app.js
+++ assets/app.js
@@ -8,58 +8,45 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
 const $ = require('admin-lte/plugins/jquery/jquery.min');
 global.$ = global.jQuery = $;
 
 const ui = require('admin-lte/plugins/jquery-ui/jquery-ui.min')
 require('admin-lte/plugins/bootstrap/js/bootstrap.bundle.min');
 
+// Datatables
 $.fn.dataTable = $.fn.DataTable = global.DataTable = require('admin-lte/plugins/datatables/jquery.dataTables.min');
 import 'admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.min'
 require( 'admin-lte/plugins/datatables-responsive/js/dataTables.responsive.min');
 require( 'admin-lte/plugins/datatables-responsive/js/responsive.bootstrap4.min');
-
-// const buttons = require('admin-lte/plugins/datatables-buttons/js/dataTables.buttons.min');
-// import 'admin-lte/plugins/datatables-buttons/js/dataTables.buttons.min';
-// require('admin-lte/plugins/datatables-buttons/js/dataTables.buttons.min')
-//// const dtbtb = require('admin-lte/plugins/datatables-buttons/js/buttons.bootstrap4.min');
-// import 'admin-lte/plugins/datatables-buttons/js/buttons.bootstrap4.min';
 require( 'admin-lte/plugins/datatables-buttons/js/buttons.bootstrap4.min');
-//// const zip = require('admin-lte/plugins/jszip/jszip.min');
-//// const pdf = require('admin-lte/plugins/pdfmake/pdfmake.min');
-//// const vfont = require('admin-lte/plugins/pdfmake/vfs_fonts');
-//// const htm = require('admin-lte/plugins/datatables-buttons/js/buttons.html5.min');
-////require('admin-lte/plugins/datatables-buttons/js/buttons.html5.min');
-//import buttons from 'admin-lte/plugins/datatables-buttons/js/dataTables.buttons.min'
-//// const print = require('admin-lte/plugins/datatables-buttons/js/buttons.print.min')
-//import print from 'admin-lte/plugins/datatables-buttons/js/buttons.print.min';
-
-//// const col = require('admin-lte/plugins/datatables-buttons/js/buttons.colVis.min');
-
-
+require('admin-lte/plugins/datatables-buttons/js/buttons.print.min');
+require('admin-lte/plugins/datatables-buttons/js/buttons.colVis.min');
 window.JSZip = require( 'jszip' );
-// require( 'datatables.net-bs4' );
-// require( 'datatables.net-buttons-bs4' );
 require( 'datatables.net-buttons/js/buttons.html5.js' );
 require( 'datatables.net-buttons/js/buttons.print.js' );
-// require( 'datatables.net-responsive-bs4' );
-//
 // pdfMake
-var pdfMake = require('pdfmake/build/pdfmake.js');
-var pdfFonts = require('pdfmake/build/vfs_fonts.js');
+const pdfMake = require('pdfmake/build/pdfmake.js');
+const pdfFonts = require('pdfmake/build/vfs_fonts.js');
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
+import * as dtLocaleEu from './lib/datatables/locales/eu.json';
+import * as dtLocaleEs from './lib/datatables/locales/es.json';
 
 const adminlte = require('admin-lte');
-
-
 
 $(function () {
     $('#btnSaveButton').on('click', function () {
         $('#crudSubmitButton').trigger('click');
     });
 
+    const appLocale = $('#appLocale').val()
+
+
     $('#myDatatable').DataTable({
+        language: appLocale === "eu" ? dtLocaleEu : dtLocaleEs,
         "dom": 'Bfrtip',
         "autoWidth": false,
         "lengthChange": true,
@@ -68,7 +55,7 @@ $(function () {
         "paging": true,
         "responsive": true,
         "searching": true,
-        "buttons": ["copy", "csv", "excel", "pdf"]
+        "buttons": ["copy", "csv", "excel", "pdf", "print","colvis"]
     });
         //
         // $('#myDatatable').DataTable(

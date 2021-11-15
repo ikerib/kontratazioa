4eb69efbe215c8d1f96affcfddae43b855574d29
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2021-11-15 15:50:22 +0100

    LDAP Authentication

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,66 +4,112 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User implements UserInterface
 {
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private ?int $id;
 
     /**
-     * @ORM\Column(type="string", length=180, unique=true)
+     * @ORM\Column(type="string", length=180, unique=true, nullable=true)
      */
-    private $username;
+    private string $username;
 
     /**
-     * @ORM\Column(type="json")
+     * @ORM\Column(type="json", nullable=true)
      */
-    private $roles = [];
+    private array $roles = [];
 
     /**
-     * @var string The hashed password
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $password;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+    private ?string $deparment;
 
     /**
-     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
-    public function getUsername(): string
-    {
-        return (string) $this->username;
-    }
-
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
-
-        return $this;
-    }
+    private ?string $displayname;
 
     /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
-    public function getUserIdentifier(): string
-    {
-        return (string) $this->username;
-    }
+    private ?string $dn;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private ?bool $enabled;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $hizkuntza;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $lanpostua;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $ldapsaila;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private ?string $nan;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private ?string $notes;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private ?bool $sailburuada;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $surname;
+
+    /**
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private array $ldapTaldeak = [];
+
+    /**
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private array $ldapRolak = [];
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Password;
+    /************************************************************************************************************************************************************/
+    /************************************************************************************************************************************************************/
+    /************************************************************************************************************************************************************/
 
     /**
      * @see UserInterface
@@ -72,7 +118,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = 'ROLE_PASAIA';
 
         return array_unique($roles);
     }
@@ -85,37 +131,240 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
     /**
-     * @see PasswordAuthenticatedUserInterface
+     * @inheritDoc
      */
-    public function getPassword(): string
+    public function getUsername(): string
     {
-        return $this->password;
+        return $this->username;
     }
 
-    public function setPassword(string $password): self
+    /**
+     * @inheritDoc
+     */
+    public function getPassword()
     {
-        $this->password = $password;
+        // TODO: Implement getPassword() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function eraseCredentials(): void
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function __toString()
+    {
+        return $this->getUsername();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
 
         return $this;
     }
 
-    /**
-     * Returning a salt is only needed, if you are not using a modern
-     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
-     *
-     * @see UserInterface
-     */
-    public function getSalt(): ?string
+    public function getDeparment(): ?string
     {
-        return null;
+        return $this->deparment;
     }
 
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials()
+    public function setDeparment(?string $deparment): self
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        $this->deparment = $deparment;
+
+        return $this;
     }
+
+    public function getDisplayname(): ?string
+    {
+        return $this->displayname;
+    }
+
+    public function setDisplayname(?string $displayname): self
+    {
+        $this->displayname = $displayname;
+
+        return $this;
+    }
+
+    public function getDn(): ?string
+    {
+        return $this->dn;
+    }
+
+    public function setDn(?string $dn): self
+    {
+        $this->dn = $dn;
+
+        return $this;
+    }
+
+    public function getEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(?bool $enabled): self
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getHizkuntza(): ?string
+    {
+        return $this->hizkuntza;
+    }
+
+    public function setHizkuntza(?string $hizkuntza): self
+    {
+        $this->hizkuntza = $hizkuntza;
+
+        return $this;
+    }
+
+    public function getLanpostua(): ?string
+    {
+        return $this->lanpostua;
+    }
+
+    public function setLanpostua(?string $lanpostua): self
+    {
+        $this->lanpostua = $lanpostua;
+
+        return $this;
+    }
+
+    public function getLdapsaila(): ?string
+    {
+        return $this->ldapsaila;
+    }
+
+    public function setLdapsaila(?string $ldapsaila): self
+    {
+        $this->ldapsaila = $ldapsaila;
+
+        return $this;
+    }
+
+    public function getNan(): ?string
+    {
+        return $this->nan;
+    }
+
+    public function setNan(?string $nan): self
+    {
+        $this->nan = $nan;
+
+        return $this;
+    }
+
+    public function getNotes(): ?string
+    {
+        return $this->notes;
+    }
+
+    public function setNotes(?string $notes): self
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    public function getSailburuada(): ?bool
+    {
+        return $this->sailburuada;
+    }
+
+    public function setSailburuada(?bool $sailburuada): self
+    {
+        $this->sailburuada = $sailburuada;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getLdapTaldeak(): ?array
+    {
+        return $this->ldapTaldeak;
+    }
+
+    public function setLdapTaldeak(?array $ldapTaldeak): self
+    {
+        $this->ldapTaldeak = $ldapTaldeak;
+
+        return $this;
+    }
+
+    public function getLdapRolak(): ?array
+    {
+        return $this->ldapRolak;
+    }
+
+    public function setLdapRolak(?array $ldapRolak): self
+    {
+        $this->ldapRolak = $ldapRolak;
+
+        return $this;
+    }
+
+    public function setPassword(?string $Password): self
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+
+
 }
--- src/Security/FormLoginAuthenticator.php
+++ src/Security/FormLoginAuthenticator.php
@@ -2,11 +2,14 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
+use App\Service\PasaiaLdapService;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -14,6 +17,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class FormLoginAuthenticator extends AbstractLoginFormAuthenticator
@@ -23,25 +27,44 @@ class FormLoginAuthenticator extends AbstractLoginFormAuthenticator
     public const LOGIN_ROUTE = 'app_login';
 
     private UrlGeneratorInterface $urlGenerator;
+    private PasaiaLdapService $pasaiaLdapSrv;
+    private UserRepository $userRepository;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator)
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        UserRepository $userRepository,
+        PasaiaLdapService $pasaiaLdapSrv
+    )
     {
         $this->urlGenerator = $urlGenerator;
+        $this->pasaiaLdapSrv = $pasaiaLdapSrv;
+        $this->userRepository = $userRepository;
     }
 
     public function authenticate(Request $request): PassportInterface
     {
-        $username = $request->request->get('username', '');
-
+        $password = $request->request->get('password');
+        $username = $request->request->get('username');
         $request->getSession()->set(Security::LAST_USERNAME, $username);
+        $result =  $this->pasaiaLdapSrv->checkCredentials($username, $password);
 
-        return new Passport(
-            new UserBadge($username),
-            new PasswordCredentials($request->request->get('password', '')),
-            [
-                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-            ]
-        );
+        if ( $result ) {
+            $dbUser = $this->userRepository->findOneBy(['username' => $username]);
+            // todo: check if the user is withing ldap groups
+            if (!$dbUser) {
+                // User is not present in the Database, let's create it
+                $this->pasaiaLdapSrv->createDbUserFromLdapData($username);
+            } else {
+                // The User exists in the database, let's update it's data
+                $this->pasaiaLdapSrv->updateDbUserDataFromLdapByUsername($username);
+            }
+        } else {
+            throw new UserNotFoundException();
+        }
+
+        return new SelfValidatingPassport(new UserBadge($username));
+
+
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
@@ -50,9 +73,8 @@ class FormLoginAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('home'));
+
     }
 
     protected function getLoginUrl(Request $request): string

a2e6aad949e39fcd37b4fb75c3a6dc0a79ec0860
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2021-12-16 07:44:49 +0100

    Email send checker

---
+++ src/Command/MailCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class MailCommand extends Command
+{
+    protected static $defaultName = 'app:mail';
+    protected static $defaultDescription = 'Add a short description for your command';
+
+    private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $email = (new Email())
+            ->from('iibarguren@pasaia.net')
+            ->to('iibarguren@pasaia.net')
+            ->subject('Jakinarazpen berria. Oroigarria')
+            ->text('Froga bat da');
+
+        $this->mailer->send($email);
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
